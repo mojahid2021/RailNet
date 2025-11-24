a98877c44a11ba6989f1b596dc2f46260699c4bc
Md Mojahid <aammojahid@gmail.com>
2025-11-24 19:37:39 +0600

    feat: add .gitignore and update deviceStreaming.xml with new device configurations

--- backend/src/prisma/migrations/20251120024052_add_route_fields_to_stations/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "stations" ADD COLUMN     "arrivalTime" TEXT,
-ADD COLUMN     "departureTime" TEXT,
-ADD COLUMN     "distance" DOUBLE PRECISION,
-ADD COLUMN     "duration" TEXT,
-ADD COLUMN     "nextStationId" TEXT,
-ADD COLUMN     "prevStationId" TEXT;
