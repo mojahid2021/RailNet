ae6699374e8d729d24bd1445e1ff6cf655658b45
Md Mojahid <aammojahid@gmail.com>
2025-12-06 03:44:28 +0600

    feat: Implement login and registration UI; update layouts and add string resources

--- android/app/src/main/java/com/mojahid2021/railnet/auth/RegisterActivity.java
+++ android/app/src/main/java/com/mojahid2021/railnet/auth/RegisterActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.mojahid2021.railnet.R;
 
 public class RegisterActivity extends AppCompatActivity {
